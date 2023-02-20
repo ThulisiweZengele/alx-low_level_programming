1560def4fd3ed4f6d99ea6cd606ff6de4e015144
ThulisiweZengele <thulisiwe.zengele@gmail.com>
2023-02-20 09:41:02 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
- *
- * Return: Always 0.
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
-	int num;
+	int d, p;
 
-	for (num = 0; num <= 9; num++)
+	for (d = '0'; d < '9'; d++)
 	{
-		putchar((num % 10) + '0');
-		if (num == 9)
-			continue;
-
-		putchar(',');
-		putchar(' ');
+	for (p = d + 1; p <= '9'; p++)
+	{
+	if (p != d)
+	{
+	putchar(d);
+	putchar(p);
+	if (d == '8' && p == '9')
+	continue;
+	putchar(',');
+	putchar(' ');
 	}
-
+	}
+	}
 	putchar('\n');
-
 	return (0);
 }
