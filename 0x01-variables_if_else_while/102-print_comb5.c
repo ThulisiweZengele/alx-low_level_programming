080bb97cf67beb24e132240a033ffe0bf7f7e5ee
ThulisiweZengele <thulisiwe.zengele@gmail.com>
2023-02-20 16:28:17 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -22,9 +22,6 @@ int main(void)
 	putchar(' ');
 	putchar((q / 10) + '0');
 	putchar((q % 10) + '0');
-	putchar(' ');
-	putchar((q / 10) + '0');
-	putchar((q % 10) + '0');
 	if (p == 98 && q == 99)
 	continue;
 	putchar(',');
